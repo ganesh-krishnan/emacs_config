4f4a80558624efac03a75b6a4be1891bb3456b22
ganesh_krishnan <ganesh.krishnan@instacart.com>
2018-04-08 23:37:38 -0700

    Add hydra for hideshow mode

--- hydras.el
+++ hydras.el
@@ -61,3 +61,26 @@ _SPC_ cancel	_o_nly this   	_d_elete
    ("SPC" nil)
    )
  )
+
+(defhydra hydra-hs (:idle 1.0)
+   "
+Hide^^            ^Show^            ^Toggle^    ^Navigation^
+----------------------------------------------------------------
+_h_ hide all      _s_ show all      _t_oggle    _n_ext line
+_d_ hide block    _a_ show block              _p_revious line
+_l_ hide level
+
+_SPC_ cancel
+"
+   ("s" hs-show-all)
+   ("h" hs-hide-all)
+   ("a" hs-show-block)
+   ("d" hs-hide-block)
+   ("t" hs-toggle-hiding)
+   ("l" hs-hide-level)
+   ("n" forward-line)
+   ("p" (forward-line -1))
+   ("SPC" nil)
+)
+
+(define-key elpy-mode-map (kbd "C-c @") 'hydra-hs/body)
